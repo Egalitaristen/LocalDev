ccadece69941c2c81573be264f8aadd1b6475dad
MightEnlightenYou <86793055+MightEnlightenYou@users.noreply.github.com>
2024-03-27 14:53:05 +0100

    Add Footer component

---
+++ GUI/src/components/Footer.js
@@ -0,0 +1,1 @@
+import React from 'react';\n\nfunction Footer() {\n  return (\n    <footer className=\"Footer\">\n      <p>Copyright &copy; {New Date().getFullYear()} LocalDev. All rights reserved.</p>\n    </footer>\n  );\n}\n\nexport default Footer;
